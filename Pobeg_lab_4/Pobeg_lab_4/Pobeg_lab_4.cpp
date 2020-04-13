c4da11368a2b2a60b40c34e6d0c35d3220fe95c9
dmitars <dmitriy.tarasienko@gmail.com>
2020-04-13 15:32:43 +0300

    refactor: change code style

--- Pobeg_lab_4/Pobeg_lab_4/Pobeg_lab_4.cpp
+++ Pobeg_lab_4/Pobeg_lab_4/Pobeg_lab_4.cpp
@@ -2,9 +2,17 @@
 #include<Windows.h>
 #include<string>
 
+bool start_process(HANDLE event, const std::string& file_name, int i);
+bool read_message(HANDLE h_file, char*& message);
+bool rewrite_file(HANDLE h_file, int number_of_records);
+void do_work(HANDLE h_mutex, HANDLE h_file);
+
+const int SIZE_OF_BUFFER = 20;
+int number_of_records;
+
 int main()
 {
-    int number_of_records,number_of_senders;
+    int number_of_senders;
     std::string file_name;
     std::cout << "enter name of binary file\n";
     std::cin >> file_name;
@@ -21,91 +29,122 @@ int main()
                                      FILE_ATTRIBUTE_NORMAL,
                                      nullptr);
     const HANDLE h_mutex = CreateMutex(nullptr, FALSE, TEXT("file_mutex"));
-    PROCESS_INFORMATION pi;
-    STARTUPINFO si;
-    ZeroMemory(&si, sizeof(STARTUPINFO));
-    si.cb = sizeof(STARTUPINFO);
+    if (h_mutex == nullptr)
+    {
+        std::cout << "cannot create mutex";
+        return 1;
+    }
 
-    auto* events = new HANDLE[number_of_senders];        
-    
+    auto* events = new HANDLE[number_of_senders];           
     for (int i = 0; i < number_of_senders; i++)
     {
-        if (!CreateProcess("D:\\ОС\\Sender\\Debug\\Sender.exe",
-            LPSTR((file_name+" "+std::to_string(number_of_records)).c_str()),
-            nullptr,
-            nullptr,
-            FALSE,
-            CREATE_NEW_CONSOLE|CREATE_SUSPENDED,
-            nullptr,
-            nullptr,
-            &si,
-            &pi))
+        if (!start_process(events[i], file_name, i))
         {
-            std::cout << "error with " << i + 1 << " sender creating\n";
-            std::cout << GetLastError()<<"\n";
-            return 1;
-        }
-        events[i] = CreateEvent(NULL, TRUE, FALSE, LPSTR(("event" + std::to_string(pi.dwProcessId)).c_str()));
-        ResumeThread(pi.hThread);
-        system("pause");
+            delete[]events;
+            CloseHandle(h_mutex);
+            CloseHandle(h_file);
+            return 2;
+        }          
     }
-    
-	if(h_mutex == NULL)
-	{
-        std::cout << "cannot create mutex";
-        return 2;
-	}
+	
     WaitForMultipleObjects(number_of_senders, events, TRUE, INFINITE);
-    
+    do_work(h_mutex, h_file);
+
+    CloseHandle(h_mutex);
+    CloseHandle(h_file);
+    delete[]events;
+    return 0;
+}
+
+void do_work(HANDLE h_mutex, HANDLE h_file)
+{
     int flag;
     char* message = new char[20];
-    char* all_file = new char[20*number_of_records];
-    char* temp_line = new char[20];
-    DWORD bytes_read;
     while (true) {
         std::cout << "enter 0 to read message or 1 to exit process\n";
         std::cin >> flag;
         if (flag == 0)
         {
             WaitForSingleObject(h_mutex, INFINITE);
-            SetFilePointer(h_file, 0, nullptr, FILE_BEGIN);
-            const DWORD file_size = GetFileSize(h_file, nullptr);
-        	if (file_size == 0ul)
-        	{
-                std::cout << "file is empty\n";
+            if (!read_message(h_file, message))
                 continue;
-        	}
-        	if(!ReadFile(h_file, message, 20, nullptr, nullptr))
-        	{
-                std::cout << "error of reading\n";
-                continue;
-        	}
-            std::cout << message<<"\n";
-            int count_of_lines = 0;
-            bytes_read = -1;
-        	while (true)
-        	{
-                if (!ReadFile(h_file, temp_line, 20, &bytes_read, nullptr))
-                {
-                    std::cout << "error of reading";
-                    continue;
-                }
-                if (bytes_read == 0)
-                    break;
-                memcpy_s(all_file + 20 * count_of_lines, 20, temp_line, 20);
-                count_of_lines++;
-        	}
-            SetFilePointer(h_file, 0, nullptr, FILE_BEGIN);
-            WriteFile(h_file, all_file, count_of_lines * 20, nullptr, nullptr);
-            SetEndOfFile(h_file);
+            std::cout << message << "\n";
 
+            if (!rewrite_file(h_file, number_of_records))
+                continue;
             ReleaseMutex(h_mutex);
         }
         else
         {
-            CloseHandle(h_mutex);
-            return 0;
+            return;
         }
     }
+}
 
+bool read_message(HANDLE h_file, char*& message)
+{
+    SetFilePointer(h_file, 0, nullptr, FILE_BEGIN);
+    const DWORD file_size = GetFileSize(h_file, nullptr);
+    if (file_size == 0ul)
+    {
+        std::cout << "file is empty\n";
+        return false;
+    }
+    if (!ReadFile(h_file, message, 20, nullptr, nullptr))
+    {
+        std::cout << "error of reading\n";
+        return false;
+    }
+    return true;
+}
+
+bool start_process(HANDLE event, const std::string& file_name, int i)
+{
+
+    PROCESS_INFORMATION pi;
+    STARTUPINFO si;
+    ZeroMemory(&si, sizeof(STARTUPINFO));
+    si.cb = sizeof(STARTUPINFO);
+    if (!CreateProcess("D:\\ОС\\Sender\\Debug\\Sender.exe",
+        LPSTR((file_name + " " + std::to_string(number_of_records)).c_str()),
+        nullptr,
+        nullptr,
+        FALSE,
+        CREATE_NEW_CONSOLE | CREATE_SUSPENDED,
+        nullptr,
+        nullptr,
+        &si,
+        &pi))
+    {
+        std::cout << "error with " << i + 1 << " sender creating\n";
+        std::cout << GetLastError() << "\n";
+        return false;
+    }
+    event = CreateEvent(nullptr, TRUE, FALSE, LPSTR(("event" + std::to_string(pi.dwProcessId)).c_str()));
+    ResumeThread(pi.hThread);
+    return true;
+}
+
+bool rewrite_file(HANDLE h_file, int number_of_records)
+{
+    char* all_file = new char[SIZE_OF_BUFFER * number_of_records];
+    char* temp_line = new char[SIZE_OF_BUFFER];
+    int count_of_lines = 0;
+    DWORD bytes_read = -1;
+    while (true)
+    {
+        if (!ReadFile(h_file, temp_line, SIZE_OF_BUFFER, &bytes_read, nullptr))
+        {
+            std::cout << "error of reading";
+            return false;
+        }
+        if (bytes_read == 0)
+            break;
+        memcpy_s(all_file + SIZE_OF_BUFFER * count_of_lines, SIZE_OF_BUFFER, temp_line, SIZE_OF_BUFFER);
+        count_of_lines++;
+    }
+    SetFilePointer(h_file, 0, nullptr, FILE_BEGIN);
+    WriteFile(h_file, all_file, count_of_lines * SIZE_OF_BUFFER, nullptr, nullptr);
+    SetEndOfFile(h_file);
+    return true;
 }
--- Sender/Sender/Source.cpp
+++ Sender/Sender/Source.cpp
@@ -2,12 +2,19 @@
 #include<Windows.h>
 #include<string>
 
+int get_user_choice();
+
+void doWork(HANDLE h_mutex, HANDLE h_file);
+void wait_place(HANDLE h_mutex, HANDLE h_file);
+
+const int SIZE_OF_BUFFER = 20;
+int number_of_records;
+
 int main(char*argc, char**argv)
 {
     const std::string file_name = argv[0];
-    const int number_of_records = std::strtol(argv[1],nullptr,10);
-    system("pause");
-    int flag;
+    number_of_records = std::strtol(argv[1],nullptr,10);
+    
     const auto process_id = GetCurrentProcessId();
     const auto h_event = OpenEvent(EVENT_MODIFY_STATE, FALSE, ("event" + std::to_string(process_id)).c_str());
     if(h_event == nullptr)
@@ -29,45 +36,57 @@ int main(char*argc, char**argv)
         std::cout << "some error with mutex";
         return 2;
     }
-    while(true)
+
+    doWork(h_mutex, h_file);
+}
+
+
+void doWork(HANDLE h_mutex, HANDLE h_file)
+{
+    int flag;
+    while (true)
     {
-        std::cout << "enter 0 to send message or 1 to close process\n";
-        std::cin >> flag;
-        std::cin.ignore();
-        if(flag == 0)
+        flag = get_user_choice();
+        if (flag == 0)
         {
-            char* buff = new char[20];
+            char* buff = new char[SIZE_OF_BUFFER];
             std::cout << "enter your message:\n";
-            std::cin.getline(buff,20);
-          
+            std::cin.getline(buff, SIZE_OF_BUFFER);
+
             WaitForSingleObject(h_mutex, INFINITE);
-            int high_size;
-            int low_size = GetFileSize(h_file, LPDWORD(&high_size));
-            while(low_size == 20*number_of_records)
-            {
-                ReleaseMutex(h_mutex);
-                Sleep(1000);
-                WaitForSingleObject(h_mutex, INFINITE);
-                low_size = GetFileSize(h_file, LPDWORD(&high_size));
-            }
+            wait_place(h_mutex, h_file);
 
-            SetFilePointer(h_file,0, nullptr, FILE_END);
-//            SetEndOfFile(h_file);
-
-            WriteFile(h_file, buff, 20, nullptr, nullptr);
+            SetFilePointer(h_file, 0, nullptr, FILE_END);
+            WriteFile(h_file, buff, SIZE_OF_BUFFER, nullptr, nullptr);
             ReleaseMutex(h_mutex);
         }
         else
         {
             CloseHandle(h_mutex);
             CloseHandle(h_file);
-            return 0;
+            return;
         }
     }
 }
 
-
-void doWork()
+int get_user_choice()
 {
-    
+    int flag;
+    std::cout << "enter 0 to send message or 1 to close process\n";
+    std::cin >> flag;
+    std::cin.ignore();
+    return flag;
+}
+
+void wait_place(HANDLE h_mutex, HANDLE h_file)
+{
+    int high_size;
+    int low_size = GetFileSize(h_file, LPDWORD(&high_size));
+    while (low_size == SIZE_OF_BUFFER * number_of_records)
+    {
+        ReleaseMutex(h_mutex);
+        Sleep(1000);
+        WaitForSingleObject(h_mutex, INFINITE);
+        low_size = GetFileSize(h_file, LPDWORD(&high_size));
+    }
 }
